11857ecdfec200a8de4b57249cd698f191a553eb
suraj thapa <surajthapa@pingidentity.com>
2021-06-11 15:04:29 +1000

    Update to log bufferSize & Concurrency

--- pkg/skbn/s3.go
+++ pkg/skbn/s3.go
@@ -78,7 +78,7 @@ func copyS3ToFile(src, dst string, parallel int, bufferSize int64) error {
 		return err
 	}
 
-	log.Printf("Concurrency: %s PartSize: %s", parall, bufferSize)
+	log.Printf("Concurrency: %s PartSize: %s", parallel, bufferSize)
 
 	client := s3.NewFromConfig(cfg)
 
@@ -139,7 +139,7 @@ func copyFileToS3(src, dst string, parallel int, bufferSize int64) error {
 
 	defer file.Close()
 
-	log.Printf("Concurrency: %s PartSize: %s", parall, bufferSize)
+	log.Printf("Concurrency: %s PartSize: %s", parallel, bufferSize)
 
 	uploader := manager.NewUploader(client, func(u *manager.Uploader) {
 		u.Concurrency = parallel
